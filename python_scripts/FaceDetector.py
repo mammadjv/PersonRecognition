872aef4622e30e60415d27d95395deb4d83d9f59
mohammad <mohammadjv94@gmail.com>
2017-08-21 11:47:13 +0430

    face detector module intitialized

--- python_scripts/FaceDetector.py
+++ python_scripts/FaceDetector.py
@@ -1,5 +1,5 @@
 import cv2
-
+import time
 
 class FaceDetector:
 
@@ -9,7 +9,8 @@ class FaceDetector:
     def crop_face(self, head_joints, frames):
         x_begin , y_begin = head_joints[0].get_x_image(), head_joints[0].get_y_image()
         x_end, y_end = head_joints[1].get_x_image(), head_joints[1].get_y_image()
-        rgb_frame, depth_frame = frames[0], frames[1]
+
+        rgb_frame, depth_frame = frames[0].copy(), frames[1].copy()
         cv2.circle(rgb_frame, (x_begin,y_begin), 1, (0, 255, 0), 3)
         cv2.circle(rgb_frame, (x_end, y_end), 1, (0, 255, 0), 3)
         rgb_croped = rgb_frame[y_begin:y_end, x_begin:x_end]
@@ -20,21 +21,16 @@ class FaceDetector:
     def get_faces(self, frame):
         if frame.get_number_of_persons() == 0:
             return None
+        ## init array of faces
         faces = []
         for person in frame.get_persons():
             head_joints = []
             for joint in person.get_joints():
-                x = joint.get_x_image()
-                y = joint.get_y_image()
-                rgb_frame = frame.get_rgb_frame()
-                cv2.circle(rgb_frame, (x, y), 1, (0, 255, 0), 3)
-                cv2.imshow('rgb_frame',rgb_frame)
-                cv2.waitKey(1)
-
-                # if joint.get_joint_name() == 'head':
-                #   head_joints.append(joint)
-                #if joint.get_joint_name() == 'neck':
-                #    head_joints.append(joint)
-                #if len(head_joints) == 2:
-                #    faces.append(self.crop_face(head_joints, frame.get_frames()))
-                #    break
+                if joint.get_joint_name() == 'head':
+                  head_joints.append(joint)
+                if joint.get_joint_name() == 'torso':
+                   head_joints.append(joint)
+                if len(head_joints) == 2:
+                   ## detect faces in image
+                   faces.append(self.crop_face(head_joints, frame.get_frames()))
+                   break
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,11 +1,16 @@
 import user_tracker as ut
 import FaceDetector as fd
+import time as time
 
-user_tracker = ut.UserTracker("./dataset/rgb_2.avi",
-                              "./dataset/depth_2.avi",
-                              "./dataset/data_2.txt")
+
+user_tracker = ut.UserTracker("./dataset/rgb_morteza.avi",
+                              "./dataset/depth_morteza.avi",
+                              "./dataset/data_morteza.txt")
 face_detector = fd.FaceDetector()
 
+begining_time = 0
 while user_tracker.frame_exist():
     face_detector.get_faces(user_tracker.get_current_frame())
+    print time.time() - begining_time
     user_tracker.show_frames()
+    begining_time = time.time()
